cd33170bbfbee0f6c05f3c088c4f79cab557c4bf
Matheus Ramos <mramos@rootbox.tech>
2023-07-09 18:04:12 -0300

    Environment variables added

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as knexSetup, Knex } from "knex";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("No database URL found on environment");
+}
+
 export const config: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: "./db/app.db",
+    filename: process.env.DATABASE_URL,
   },
   migrations: {
     directory: "./db/migrations",
